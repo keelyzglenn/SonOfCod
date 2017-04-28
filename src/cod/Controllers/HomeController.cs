de2eed73d215581bfcef9847c2fd4dab1047cd41
Jessica Glenn <glenj392@newschool.edu>
2017-04-28 12:07:38 -0700

    Allow subscribers to add to news lettergit add .!

---
+++ src/cod/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using cod.Models;
+
+
+namespace cod.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SonOfCodSeafoodContext db = new SonOfCodSeafoodContext();
+        public IActionResult Index()
+        {
+            return View(db.Subscribers.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Subscriber subscriber)
+        {
+            db.Subscribers.Add(subscriber);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+
+
+}
