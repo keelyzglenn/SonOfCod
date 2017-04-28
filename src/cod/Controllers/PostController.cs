365a6ba7557b2d10c3aa1444a61ae4af4ab2c5ec
Jessica Glenn <glenj392@newschool.edu>
2017-04-28 13:30:44 -0700

    add edit and delete for posts

--- src/cod/Controllers/PostController.cs
+++ src/cod/Controllers/PostController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using cod.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace cod.Controllers
 {
@@ -29,7 +29,35 @@ namespace cod.Controllers
             return RedirectToAction("Index");
         }
 
-    }
 
+        public IActionResult Edit(int id)
+        {
+            var thisPost = db.Posts.FirstOrDefault(posts => posts.PostId == id);
+            return View(thisPost);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Post post)
+        {
+            db.Entry(post).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisPost = db.Posts.FirstOrDefault(items => items.PostId == id);
+            return View(thisPost);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisPost = db.Posts.FirstOrDefault(products => products.PostId == id);
+            db.Posts.Remove(thisPost);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
 
 }
